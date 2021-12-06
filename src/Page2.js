f7b5d4a011b403877bbf6ae76c6b38bf3ddb0b4c
jmoney2001 <81720235+jmoney2001@users.noreply.github.com>
2021-12-05 21:40:44 -0500

    Update Page2.js

--- src/Page2.js
+++ src/Page2.js
@@ -26,6 +26,7 @@ function Page2() {
           <p>
             First one must install node.js, which allows you to execute
             JavaScript without a browser.
+          </p>
             <p>
               1. Search on Google: "Node.js" and click on the first thing that
               pops up
@@ -36,7 +37,6 @@ function Page2() {
               the one that is "Recommended For Most Users" and proceed with the
               installation
             </p>
-          </p>
           <br/>
           <h3>Setting Up Visual Studio Code</h3>
           <p>
@@ -57,13 +57,13 @@ function Page2() {
             2. Click the download button with whatever operating system you are
             on and proceed with the installation
           </p>
-          <br/>
+          /*<br/>*/
           <h3>Setting Up React</h3>
           <p>1. To set up React, just open Visual Studio Code with a terminal open (PowerShell or Bash for example)</p>
           <p>2. Be in the directory where you want your React Application to be in</p>
           <p>3. Then type in the terminal "npx create-react-app my_app" *you can change my_app into any name you want your application to be*</p>
           <p>4. Once everything is done installing, type in the terminal "npm start". This then should open up your React application in your browser in "http://localhost:3000"</p>
-          <br/>
+          /*<br/>*/
         </Typography>
       </ThemeProvider>
     </div>
